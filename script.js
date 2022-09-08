f406007afbd4b0627aebc2ae0cc50767c2bdadf6
Marquell <starjet164@gmail.com>
2022-09-08 16:04:18 -0500

    added all questions in js

---
+++ script.js
@@ -0,0 +1,70 @@
+//Questions That will be asked
+const Questions = [{
+    id: 0,
+    q: "What killed gwen stacy in classic Amzing Spider-man #122",
+    a: [{ text: "the green goblin throwing her off a bridge", isCorrect: false },
+    { text: "Spideriman shooting out a web and catching her but snaping her neck", isCorrect: true },
+    { text: "Venom eating her", isCorrect: false },
+    { text: "the joker blowing her up", isCorrect: false }
+    ]
+},
+{
+    id: 1,
+    q: "In Batman:Knightfall who broke the bat",
+    a: [{ text: "The Joker", isCorrect: false },
+    { text: "The Redhood", isCorrect: false },
+    { text: "Bane", isCorrect: true },
+    { text: "Deathstroke", isCorrect: false }
+    ]
+},
+{
+    id: 2,
+    q: "How many realities did Dr.Strange go throught in Infinity War",
+    a: [{ text: " 14,000,605", isCorrect: true },
+    { text: "50,000,000", isCorrect: false },
+    { text: "50", isCorrect: false },
+    { text: "1,000,000,000", isCorrect: false }
+    ]
+},
+{
+    id: 3,
+    q: "What is the name of batman's son",
+    a: [{ text: "Jason Todd", isCorrect: false },
+    { text: "Robin", isCorrect: false },
+    { text: "Dick Grayson", isCorrect: false },
+    { text: "Damian Wayne", isCorrect: true }
+    ]
+},
+{
+    id: 4,
+    q: "Who is loki's brother",
+    a: [{ text: "Satan", isCorrect: false },
+    { text: "Iron Man", isCorrect: true },
+    { text: "Dr.Strange", isCorrect: false },
+    { text: "Thor", isCorrect: false }
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
